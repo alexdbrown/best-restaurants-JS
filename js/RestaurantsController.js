36c3941a562d56dccaf3646dc98c6fc87e9e52e3
Phillip Shannon & Alexandra Brown <student@epicodus.com>
2015-09-21 12:26:51 -0700

    add fire for intimacy rating

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,11 +1,25 @@
 restaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope)  {
   $scope.bestRestaurants = [];
   $scope.addRestaurant = function() {
-    $scope.bestRestaurants.push({ name: $scope.restaurantName });
+    $scope.bestRestaurants.push(
+      { name: $scope.restaurantName,
+        description: $scope.restaurantDescription,
+        price: $scope.restaurantPrice,
+        intimacy: $scope.restaurantIntimacy,
+        sickness: $scope.restaurantSickScale }
+    );
     $scope.restaurantName = null;
+    $scope.restaurantDescription = null;
+    $scope.restaurantPrice = null;
+    $scope.restaurantIntimacy = null;
+    $scope.restaurantSickScale = null;
   };
   $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.bestRestaurants.indexOf(restaurant);
     $scope.bestRestaurants.splice(index, 1);
   };
+
+  $scope.getNumber = function(num) {
+    return new Array(num);
+  }
 });
