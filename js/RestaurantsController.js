3a84ff8b40db38f328fdbfae10d355abaefc0181
Phillip Shannon & Alexandra Brown <student@epicodus.com>
2015-09-21 10:59:08 -0700

    add delete option

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -4,4 +4,8 @@ restaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope)  {
     $scope.bestRestaurants.push({ name: $scope.restaurantName });
     $scope.restaurantName = null;
   };
+  $scope.deleteRestaurant = function(restaurant) {
+    var index = $scope.bestRestaurants.indexOf(restaurant);
+    $scope.bestRestaurants.splice(index, 1);
+  };
 });
